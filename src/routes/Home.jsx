cf280a434b229f8bd40145361492a394ed984e11
pujan <pujxnp@gmail.com>
2023-05-08 23:20:36 +0530

    Added styling to homepage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,19 +1,19 @@
 const Home = () => {
     return (
-        <div>
+        <div className="w-4/5 ml-32">
             <div>
-                <p>Hi, my name is</p>
-                <p>Pujan Parikh.</p>
-                <p>I craft experiences for the web.</p>
+                <p className="text-light-orange text-2xl">Hi, my name is</p>
+                <p className="text-white font-bold text-5xl">Pujan Parikh.</p>
+                <p className="text-light-gray font-bold text-4xl">I craft experiences for the web.</p>
             </div>
-            <div>
-                <p>I'm a software developer with a passion for human-centered design.</p>
-                <p>I'm currently looking for opportunites with motivated teams to build delightful and performant experiences for the user.</p>
+            <div className="text-2xl w-3/5">
+                <p className="text-light-gray">I'm a <span className="text-light-orange">software developer</span> with a passion for human-centered design.</p>
+                <p className="text-light-gray">I'm currently <span className="text-light-orange">looking for opportunites</span> with motivated teams to build delightful and performant experiences for the user.</p>
             </div>
-            <div>
-                <button>Get in touch!</button>
+            <div className="w-full">
+                <button className="mt-16 text-2xl border-2 border-light-orange text-light-orange">Get in touch!</button>
             </div>
-        </div>
+        </div >
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "deep-purple": "#5C6BC0",
+        "light-orange": "#FFB74D",
+        "light-gray": "#E0E0E0"
+      },
+      fontFamily: {
+        "courier-prime": ['Courier Prime', "monospace"]
+      }
     },
-    plugins: [],
+  },
+  plugins: [],
 }
+
