e6b2efe68308709826aaaf647749d6e106abc693
pujan <pujxnp@gmail.com>
2023-05-08 21:14:00 +0530

    Added react router, tailwind, removed initial vite config files, set up some routes and componenets, populated Home route jsx

---
+++ src/routes/Home.jsx
@@ -0,0 +1,20 @@
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <p>Hi, my name is</p>
+                <p>Pujan Parikh.</p>
+                <p>I craft experiences for the web.</p>
+            </div>
+            <div>
+                <p>I'm a software developer with a passion for human-centered design.</p>
+                <p>I'm currently looking for opportunites with motivated teams to build delightful and performant experiences for the user.</p>
+            </div>
+            <div>
+                <button>Get in touch!</button>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
