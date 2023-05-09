119b182751bd3f8aad5808f7de772e90ff11ea79
pujan <pujxnp@gmail.com>
2023-05-09 14:08:30 +0530

    Added button animation

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -13,6 +13,11 @@ const Home = () => {
         visible: { opacity: 1, transition: { staggerChildren: 0.03 } }
     }
 
+    const buttonVariant = {
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0, transition: { ease: "easeIn", duration: 1 } }
+    }
+
     const splitToSpan = ({ str, ...args }) => {
         return str.split("").map((ele) => {
             return <motion.span key={uuidv4()} variants={itemVariant} {...args}>{ele}</motion.span>
@@ -28,10 +33,11 @@ const Home = () => {
             </div>
             <div className="text-2xl w-3/5">
                 <p className="text-light-gray">{splitToSpan({ str: "I'm a " })}{splitToSpan({ str: "software developer", className: "text-light-orange" })}{splitToSpan({ str: " with a passion for human-centered design." })}</p>
-                <p className="text-light-gray">{splitToSpan({ str: "I'm currently " })}{splitToSpan({ str: "looking for opportunites", className: "text-light-orange" })}{splitToSpan({ str: " with motivated teams to build delightful and performant experiences for the user." })}</p>
+                <p className="text-light-gray">{splitToSpan({ str: "At the moment, I'm " })}{splitToSpan({ str: "looking for opportunites", className: "text-light-orange" })}{splitToSpan({ str: " with motivated teams to build delightful and performant experiences for the user." })}</p>
             </div>
             <div className="w-full">
-                <button className="shadow-xl p-3 mt-16 text-2xl border-2 border-light-orange text-light-orange">Get in touch!</button>
+                <motion.button variants={buttonVariant} className="shadow-xl p-3 mt-16 text-2xl border-2 border-light-orange text-light-orange">Get in touch!</motion.button>
+
             </div>
         </motion.div >
     )
