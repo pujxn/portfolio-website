38ec6c295e3de8cc90c2e397492c4869ee3efc73
pujan <pujxnp@gmail.com>
2023-05-18 01:14:23 +0530

    Added anchoring from 1rem instead of 0

--- src/components/mobile components/Navbar Components/HamburgerMenuList.jsx
+++ src/components/mobile components/Navbar Components/HamburgerMenuList.jsx
@@ -2,7 +2,7 @@ import objectHash from "object-hash";
 
 const HamburgerMenuList = ({ listItems, handleClose }) => {
     return (
-        <div className="absolute top-0 right-0 text-right">
+        <div className="absolute top-4 right-4 text-right">
             <ul>
                 {listItems.map(ele => <li key={objectHash(ele)}>{ele}</li>)}
                 <li><button onClick={handleClose}>Close</button></li>
