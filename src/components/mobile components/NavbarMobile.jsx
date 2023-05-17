f1ab66280729a9b4a19405b2fe8811fcb4ab6126
pujan <pujxnp@gmail.com>
2023-05-18 01:48:53 +0530

    Added line height to hamburger list

--- src/components/mobile components/NavbarMobile.jsx
+++ src/components/mobile components/NavbarMobile.jsx
@@ -2,7 +2,7 @@ import HamburgerMenu from "@/components/mobile components/Navbar Components/Hamb
 
 const NavbarMobile = () => {
     return (
-        <div className="h-1/10 flex justify-between items-center px-4 text-sm relative">
+        <div className="h-1/10 flex justify-between items-center text-sm relative px-4">
             <div className="text-light-orange">
                 <p>Pujan Parikh</p>
             </div>
