12a420b27758236f3f7e1e46e374086b9e417de1
pujan <pujxnp@gmail.com>
2023-05-18 12:38:48 +0530

    Changed hamburger menu layout

--- src/components/mobile components/HomeMobile.jsx
+++ src/components/mobile components/HomeMobile.jsx
@@ -11,15 +11,21 @@ const HomeMobile = () => {
     }
 
     return (
-        <div className="h-9/10 grid grid-rows-3 px-4">
-            <div className="row-start-1">
+        // <div className="h-9/10 grid grid-rows-3 px-4">
+        <div className="h-9/10 px-4">
+
+            {/* <div className="row-start-1"> */}
+            <div className="h-1/3">
                 <ProfileImage />
             </div>
-            <motion.div className="row-start-2 row-span-2 grid grid-rows-2" variants={containerVariant} initial="hidden" animate="visible">
-                <div className="row-start-1 row-span-1 ">
+            {/* <motion.div className="row-start-2 row-span-2 grid grid-rows-2" variants={containerVariant} initial="hidden" animate="visible"> */}
+            <motion.div className="h-2/3" variants={containerVariant} initial="hidden" animate="visible">
+                {/* <div className="row-start-1 row-span-1 "> */}
+                <div className="h-1/2">
                     <Text />
                 </div>
-                <div className="row-start-2 row-span-1">
+                {/* <div className="row-start-2 row-span-1"> */}
+                <div className="h-1/2">
                     <Button />
                 </div>
             </motion.div>
--- src/components/mobile components/Navbar Components/HamburgerMenuList.jsx
+++ src/components/mobile components/Navbar Components/HamburgerMenuList.jsx
@@ -3,11 +3,11 @@ import { AiOutlineClose } from "react-icons/ai"
 
 const HamburgerMenuList = ({ listItems, handleClose }) => {
     return (
-        <div className="absolute top-4 right-0 text-right backdrop-blur-xl border-4 border-white">
+        <div className="absolute top-4 right-0 text-left backdrop-blur-xl border-4 border-white">
             <ul className=" leading-10">
-                <li className="text-left"><button onClick={handleClose}><AiOutlineClose className="text-light-orange" /></button></li>
+                <li className="text-right pr-2"><button onClick={handleClose}><AiOutlineClose className="text-light-orange" /></button></li>
                 <hr />
-                {listItems.map(ele => <li key={objectHash(ele)} className="pr-4 text-white">{ele}</li>)}
+                {listItems.map(ele => <li key={objectHash(ele)} className="pl-2 text-white">{ele}</li>)}
             </ul>
         </div>
     )
